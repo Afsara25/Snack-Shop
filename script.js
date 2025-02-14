9942d0b0334335c419ebbb44406454a5ec8f4548
Afsara25 <afsara.adiba@gmail.com>
2025-02-13 23:30:08 -0500

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Change background color when Buy Now button is clicked
 document.querySelectorAll("button").forEach(button => {
     button.addEventListener("click", () => {
         alert("Item added to cart!");
