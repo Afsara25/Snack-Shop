320f223c381565241fa860340765f40de7ac270d
Afsara25 <afsara.adiba@gmail.com>
2025-02-14 00:02:11 -0500

    script.js

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ document.querySelectorAll("button").forEach(button => {
         alert("Item added to cart!");
     });
 });
+
+document.querySelector("form").addEventListener("submit", function (event) {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+
+    if (!name || !email) {
+        alert("Please fill in all fields.");
+        event.preventDefault();
+    } else {
+        alert("Form submitted successfully!");
+    }
+});
